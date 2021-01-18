80763964ba5e78de9dddf4265c6ffc87b95a217d
RohanChauhan <rohan021195@gmail.com>
2021-01-19 00:06:37 +0530

    second

--- BinaryTree/binaryTree.java
+++ BinaryTree/binaryTree.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 public class binaryTree{
     Node root;
     static int idx=0;
@@ -44,5 +46,100 @@ public class binaryTree{
     private int size(Node node){
         if(node==null) return 0;
         return size(node.left)+size(node.right)+1;
+    }
+    public int maximum(){
+        return maximum(root);
+    }
+    private int maximum(Node node){
+        if(node==null) return Integer.MIN_VALUE;
+        int lm=maximum(node.left);
+        int rm=maximum(node.right);
+        return Math.max(node.data,Math.max(lm,rm));
+    }
+    public int minimum(){
+        return minimum(root);
+    }
+    private int minimum(Node node){
+        if(node==null) return Integer.MAX_VALUE;
+        int lm=minimum(node.left);
+        int rm=minimum(node.right);
+        return Math.min(node.data,Math.min(lm,rm));
+    }
+    public int height(){
+        return height(root);
+    }
+    private int height(Node node){
+        if(node==null) return 0;
+        int lh=height(node.left);
+        int rh=height(node.right);
+        return Math.min(lh,rh)+1;
+    }
+    public boolean find(int data){
+        return find(root,data);
+    }
+    private boolean find(Node node, int data){
+        if(node==null) return false;
+        if(node.data==data) return true;
+        return find(node.left,data)|| find(node.right,data);
+    }
+    public void rootToNodePath(int data){
+        ArrayList<Node>ans=rootToNodePath(root, data);
+        for(Node node: ans){
+            System.out.print(node.data+"->");
+        }
+
+    }
+    private ArrayList<Node> rootToNodePath(Node node,int data){
+        if(node==null) return null;
+        if(node.data==data){
+            ArrayList<Node>base=new ArrayList<>();
+            base.add(node);
+            return base;
+        }
+        ArrayList<Node>ls=rootToNodePath(node.left,data);
+        if(ls!=null){
+            ls.add(node);
+            return ls;
+        }
+        ArrayList<Node>rs=rootToNodePath(node.right,data);
+        if(rs!=null){
+            rs.add(node);
+            return rs;
+        }
+        return null;
+
+    }
+    public void lca01(int data1,int data2){
+        System.out.println("LCA: "+lca01(root,data1,data2).data);
+    }
+    private Node lca01(Node node,int data1,int data2){
+        ArrayList<Node>ls=rootToNodePath(node,data1);
+        ArrayList<Node>rs=rootToNodePath(node, data2);
+        if(ls==null || rs==null) return null;
+        Node ans=null;
+        int i=0,j=0;
+        while(i<ls.size() && j<rs.size()){
+            if(ls.get(i).data==rs.get(j).data){
+                ans=ls.get(i);
+                break;
+            }
+            i++;
+            j++;
+        }
+        return ans;
+    }
+    Node lca=null;
+    public void lca02(int data1,int data2){
+        System.out.println(lca02(root,data1,data2));
+    }
+    private boolean lca02(Node node,int data1,int data2){
+        if(node==null) return false;
+
+        boolean selfDone=
+        boolean ls=lca02(node.left, data1, data2);
+        boolean rs=lca02(node.right, data1, data2);
+
+
+        return ls||rs||selfDone;
     }
 }
--- BinaryTree/client.java
+++ BinaryTree/client.java
@@ -1,8 +1,15 @@
 public class client{
     public static void main(String[] args){
-        int []arr={1,2,3,-1,-1,4,-1,-1,5,6,-1,-1,7,-1,-1};
+        int []arr={10,20,40,80,-1,-1,90,-1,-1,50,-1,100,-1,-1,30,60,-1,110,-1,-1,70,-1,120,-1,-1};
         binaryTree bt=new binaryTree(arr);
-        bt.display();
-        System.out.println("Size: "+bt.size());
+        // bt.display();
+        // System.out.println("Size: "+bt.size());
+        // System.out.println("Max: "+bt.maximum());
+        // System.out.println("Min: "+bt.minimum());
+        // System.out.println("Height: "+bt.height());
+        // System.out.println("data is present in tree: "+bt.find(0));
+        bt.rootToNodePath(110);
+        System.out.println();
+        bt.lca01(120,110);
     }
 }
